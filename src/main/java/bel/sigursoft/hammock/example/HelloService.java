bf4a038b92a5fb170a6e3ef0278fc20aaaad4147
Anton Kozik <pazitron@gmail.com>
2018-05-22 15:08:39 +0200

    add Infinispan
    Simple Infinispan cache inject

--- src/main/java/bel/sigursoft/hammock/example/HelloService.java
+++ src/main/java/bel/sigursoft/hammock/example/HelloService.java
@@ -4,12 +4,20 @@ import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import javax.inject.Inject;
+import org.infinispan.Cache;
 
 @Path("/hello")
 @RequestScoped
 public class HelloService {
+
+    @Inject
+    private Cache<String, String> cache;
+
     @GET
     public Response doGet() {
-        return Response.ok("Hello, world!").build();
+        cache.put("User", "William");
+        String me = cache.get("User");
+        return Response.ok("Hello, world! " + me).build();
     }
 }
